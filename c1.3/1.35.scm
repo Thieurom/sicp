cf5b3cff385ae8f83095d40fd677b87acdf83f71
Doan Le Thieu <thieurom@gmail.com>
2017-10-25 20:15:55 +0700

    add 1.30, 1.35

---
+++ c1.3/1.35.scm
@@ -0,0 +1,5 @@
+;; If φ is a fixed point of the transformation x |-> 1 + 1/x,
+;; φ must be a root of equation x = 1 + 1/x
+;; or in quadratic form: x^2 - x - 1 = 0 
+;; Since this quadratic equation has a positive root of (1 + √5)/2,
+;; that is value of φ => q.e.d
