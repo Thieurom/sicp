37288ee00dfc8476df9eccdde6591e2b95d3bd32
Doan Le Thieu <thieurom@gmail.com>
2017-11-02 10:13:04 +0700

    add 2.02

---
+++ c2.1/2.02.scm
@@ -0,0 +1,39 @@
+;; Point
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+
+;; Segment
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (mid-segment s)
+  (make-point (average (x-point (start-segment s))
+                       (x-point (end-segment s)))
+              (average (y-point (start-segment s))
+                       (y-point (end-segment s)))))
+
+
+;; Util
+(define (average a b)
+  (/ (+ a b) 2))
